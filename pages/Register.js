2e1e0c68a0d42815edb69d1ebf1a6fd3261b4220
Subhendu4321 <subhendu4526@gmail.com>
2020-05-05 17:11:41 +0530

    form validation-1

--- pages/Register.js
+++ pages/Register.js
@@ -48,16 +48,46 @@ function Register(){
     const [confirmPassword,setConfirmPassword] = useState("");
     const [city,setCity] = useState("");
 
+    const [firstNameError,setFirstNameError] = useState("");
+    const [lastNameError,setLastNameError] = useState("");
+    const [phoneNumError,setPhoneNumError] = useState("");
+    const [emailError,setEmailError] = useState("");
+    const [passwordError,setPasswordError] = useState("");
+    const [confirmPasswordError,setConfirmPasswordError] = useState("");
+    const [cityError,setCityError] = useState("");
+
+
     // const router = useRouter();
     const classes = useStyles();
 
+    const valid = () =>{
+        
+        if(phoneNum.length !== 10){
+            setPhoneNumError("Invalid Phone Number")
+            
+        }
+        if(password.length < 5){
+            
+            setPasswordError("Password length should greater than 5 digit")
+        }
+        if(!(email.includes("@")) && !(email.includes(".com"))){
+            setEmailError("Invalid Email Address")
+            
+        }
+        if(password !== confirmPassword){
+            setConfirmPasswordError("Enter correct password")
+        }
+    }
+
 
     const doRegister = () => {
-        if (password !== confirmPassword){
-            alert("Pleaseenter correct Password");
-        }
-        else{
-            alert("Registration successful");
+        
+        setPasswordError(""),
+        setConfirmPasswordError(""),
+        setEmailError(""),
+        setPhoneNumError("");
+        if (valid()){
+            alert("Registration Successful");
             localStorage.setItem("firstName",firstName);
             localStorage.setItem("lastName",lastName);
             localStorage.setItem("phoneNum",phoneNum);
@@ -66,10 +96,27 @@ function Register(){
             localStorage.setItem("city",city);
             Router.push("/Profile")
 
-            
         }
 
     };
+    // const doRegister = () => {
+    //     if (password !== confirmPassword){
+    //         alert("Pleaseenter correct Password");
+    //     }
+    //     else{
+    //         alert("Registration successful");
+            // localStorage.setItem("firstName",firstName);
+            // localStorage.setItem("lastName",lastName);
+            // localStorage.setItem("phoneNum",phoneNum);
+            // localStorage.setItem("email",email);
+            // localStorage.setItem("password",password);
+            // localStorage.setItem("city",city);
+            // Router.push("/Profile")
+
+            
+    //     }
+
+    // };
     const doBack =() =>{
         Router.push("/Login");
 
@@ -85,24 +132,30 @@ function Register(){
                     onChange={event => setFirstName(event.target.value)}
                     
                     />
+                    
                     <TextField id="lastName" label="Last Name" variant="outlined" color="primary" className="password"style={{padding:9}}
                     onChange={event => setLastName(event.target.value)}
                     />
                     <TextField id="phoneNum" label="Phone Number" variant="outlined" color="primary" className="password"style={{padding:9}}
                     onChange={event => setPhoneNum(event.target.value)}
                     />
+                    <p style={{color:"red",fontSize:"15px"}}>{phoneNumError}</p>
                     <TextField id="city" label="City" variant="outlined" color="primary" className="city"style={{padding:9}}
                     onChange={event => setCity(event.target.value)}
                     />
+                    <p style={{color:"red",fontSize:"15px"}}>{cityError}</p>
                     <TextField id="email_id" label="Email Id" variant="outlined" color="primary" className="userid" style={{padding:9}}
                     onChange={event => setEmail(event.target.value)}
                     />
+                    <p style={{color:"red",fontSize:"15px"}}>{emailError}</p>
                     <TextField type="password" id="Password" label="Password" variant="outlined" color="primary" className="password"style={{padding:9}}
                     onChange={event => setPassword(event.target.value)}
                     />
+                    <p style={{color:"red",fontSize:"15px"}}>{passwordError}</p>
                     <TextField type="password" id="confirmPassword" label="Confirm Password" variant="outlined" color="primary" className="password"style={{padding:9}}
                     onChange={event => setConfirmPassword(event.target.value)}
                     />
+                    <p style={{color:"red",fontSize:"15px"}}>{confirmPasswordError}</p>
 
                     <div className={classes.ButtonClass}>
                         <Button variant="contained" color="primary" className={classes.button} type="submit" style={{padding:9}} onClick={doRegister}>Register</Button>
